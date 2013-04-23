5ad6f1e98754526a5041a84cf2eb5b092636a86e
tovianus <tovianus@gmail.com>
2013-04-23 16:17:32 +0800

    finish installing pristine rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StckLocal::Application.config.secret_token = '6bb42f27c8cb675c035da032694fd30fc386f9561221d66e173d81835b0be65f147f829f74086af8e7a0fe78f2ef324e47c29849a8dbb62569a26cef71fa072f'
